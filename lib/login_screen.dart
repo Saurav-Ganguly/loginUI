7c5da211a0ff1cf9d1ca9eba65878a30d9ecab0b
Saurav Ganguly <hisauravganguly@gmail.com>
2023-09-28 17:15:59 +0530

    added screenshots

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -11,47 +11,52 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            children: [
-              Image.asset('assets/images/signin_balls.png'),
-              const Text(
-                'Sign in.',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/signin_balls.png'),
+                const Text(
+                  'Sign in.',
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 50),
-              const SocialButton(
-                iconPath: 'assets/svgs/f_logo.svg',
-                labelText: 'Facebook',
-                horizontalPadding: 90,
-              ),
-              const SizedBox(height: 20),
-              const SocialButton(
-                iconPath: 'assets/svgs/g_logo.svg',
-                labelText: 'Google',
-              ),
-              const SizedBox(height: 15),
-              const Text(
-                'or',
-                style: TextStyle(
-                  fontSize: 17,
+                const SizedBox(height: 50),
+                const SocialButton(
+                  iconPath: 'assets/svgs/f_logo.svg',
+                  labelText: 'Facebook',
+                  horizontalPadding: 90,
                 ),
-              ),
-              const SizedBox(height: 15),
-              const LoginField(
-                hintText: 'Email',
-              ),
-              const SizedBox(height: 15),
-              const LoginField(
-                hintText: 'Passoword',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const GradientButton(),
-            ],
+                const SizedBox(height: 20),
+                const SocialButton(
+                  iconPath: 'assets/svgs/g_logo.svg',
+                  labelText: 'Google',
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  'or',
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+                const SizedBox(height: 15),
+                const LoginField(
+                  hintText: 'Email',
+                ),
+                const SizedBox(height: 15),
+                const LoginField(
+                  hintText: 'Passoword',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const GradientButton(),
+              ],
+            ),
           ),
         ),
       ),
